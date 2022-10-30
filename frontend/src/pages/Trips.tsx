ac5221f7e1570b5c671edfd346ef8e45785b5d7c
nflnkr <105123049+nflnkr@users.noreply.github.com>
2022-10-30 20:48:10 +0300

    100% height

--- frontend/src/pages/Trips.tsx
+++ frontend/src/pages/Trips.tsx
@@ -1,24 +1,20 @@
 import { Box, Container, List, ListItem, Typography } from "@mui/material";
-import { useContext, useEffect, useState } from "react";
 import TripCard from "../components/TripCard";
 import { useAppSelector } from "../redux/hooks";
 
 // TODO virtualized list
 // TODO sort by date/distance
 export default function Trips() {
-    const trips = useAppSelector(state => state.trips)
-    /* function sortFunction(trip1: TripMeta, trip2: TripMeta) {
-        return trip2.startDate - trip1.startDate;
-    } */
+    const trips = useAppSelector(state => state.trips);
 
     return (
-        <Container maxWidth="md">
+        <Container maxWidth="md" sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
             <Box sx={{ display: "flex", justifyContent: "space-between" }}>
                 <Typography variant="h4">My trips</Typography>
             </Box>
-            {trips && trips.length > 0 ?
+            {!!trips.length ?
                 <List>
-                    {trips/* .sort(sortFunction) */.map(trip =>
+                    {trips.map(trip =>
                         <ListItem key={trip._id} disableGutters>
                             <TripCard tripId={trip._id} height={260} />
                         </ListItem>
@@ -28,5 +24,5 @@ export default function Trips() {
                 <Typography variant="h5" sx={{ textAlign: "center", m: 3 }}>Nothing here 😞</Typography>
             }
         </Container>
-    )
+    );
 }
